278f069c7acc6c0797e2e0e118b161958023dc60
sa6oznaka <treildr@gmail.com>
2019-02-28 10:39:00 +0200

    fixed seg f.

--- OS/arr_sort.c
+++ OS/arr_sort.c
@@ -5,15 +5,34 @@
 #define NUM_OF_THREADS 100
 #define SIZE 10
 
-void* sort_number(void* arg){
-	int *arr = *((int *)arg);
-	
+int f_array[SIZE/2];
+int s_array[SIZE/2];
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+void* sort_number_1(void* arg){
+
+    qsort(f_array, SIZE/2, sizeof(int), cmpfunc);
+
 	for(int i=0;i<SIZE/2;i++){
-		printf("%d", arr[i]);
+		printf("%d\n", f_array[i]);
 	}
 	
 	return NULL;
 }
+void* sort_number_2(void* arg){
+	
+    qsort(s_array, SIZE/2, sizeof(int), cmpfunc);
+
+	for(int i=0;i<SIZE/2;i++){
+		printf("%d\n", s_array[i]);
+	}
+	
+	return NULL;
+}
+
 
 int main(){
 	int *arr_origin = malloc(sizeof(int)*SIZE);
@@ -26,16 +45,35 @@ int main(){
 	}
 	
 	for(int i=0;i<SIZE/2;i++){
-		arr_first[i] = arr_origin[i];
+		f_array[i] = arr_origin[i];
 	}
 	for(int i=SIZE/2;i<SIZE;i++){
-		arr_second[i-SIZE/2] = arr_origin[i];
+		s_array[i-SIZE/2] = arr_origin[i];
 	}
 
-	pthread_create(&threads[0],NULL,sort_number,&arr_first);
-	pthread_create(&threads[1],NULL,sort_number,&arr_second);
+	pthread_create(&threads[0],NULL,sort_number_1,NULL);
+	pthread_create(&threads[1],NULL,sort_number_2,NULL);
 	pthread_join(threads[0],NULL);
 	pthread_join(threads[1],NULL);
 	
+    int pos1 = 0,
+        pos2 = 0;
+
+    int result_arr[SIZE];
+    while(pos1 < SIZE/2 && pos2 < SIZE/2){
+        if(f_array[pos1] > s_array[pos2]){
+            result_arr[pos1 + pos2] = f_array[pos1];
+            pos1++;
+        }else{
+            result_arr[pos1 + pos2] = s_array[pos2];
+            pos2++;
+        }        
+    }
+    printf("=============\n");
+    for(int i=0;i<SIZE;i++){
+        printf("%d \n", result_arr[i]);
+    }
+
+
 	return 0;
 }
