15f0a724814318542953bb5fc0f131907f9fd46f
sa6oznaka <treildr@gmail.com>
2019-03-09 19:33:54 +0200

    Added files

--- OOP/homeworks/01-card-game/src/org/elsys/cardgame/api/BasicCard.java
+++ OOP/homeworks/01-card-game/src/org/elsys/cardgame/api/BasicCard.java
@@ -1,4 +1,19 @@
 package org.elsys.cardgame.api;
 
-public class BasicCard {
+public abstract class BasicCard implements Card{
+
+    Suit mySuit;
+    Rank myRank;
+
+    @Override
+    public Suit getSuit() {
+        return mySuit;
+    }
+
+    protected Rank getRank() {
+        return myRank;
+    }
+
+
+
 }
---
+++ OOP/homeworks/01-card-game/src/org/elsys/cardgame/api/Card.java
@@ -0,0 +1,9 @@
+package org.elsys.cardgame.api;
+
+public interface Card {
+
+	Suit getSuit();
+
+	Rank getRank();
+
+}
